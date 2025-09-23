62005bf60518a3d0a86240be3992ffbee8550a6b
mudasir ahmad <mudassirmwt788@gmail.com>
2025-09-23 18:22:36 +0500

    technolgies page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Technology from "./components/Technology";
+import Projects from "./components/Projects";
 function App() {
   return (
     <>
       <div className="overflow-x-hidden text-stone-300 antialiased">
         <div className="fixed inset-0 -z-10">
-          <div className="relative h-full w-full bg-slate-950">
-            <div
-              className="absolute bottom-0 left-0 right-0 top-0 
-            bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),
-            linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
-            bg-[size:14px_24px]
-             [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"
-            ></div>
-          </div>
+          <div className="relative h-full w-full bg-gradient-to-r from-[#000000] to-[#252525]"></div>
         </div>
 
         <div className="container mx-auto px-8">
           <Navbar />
           <Hero />
+          <Technology />
+          <Projects />
         </div>
       </div>
     </>
---
+++ src/components/Technology.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { DiRedis } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa6";
+import { RiReactjsLine, RiTailwindCssFill } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
+function Technology() {
+  return (
+    <div className="pb-24">
+      <h2 className="my-20 text-center text-4xl "> Technologies</h2>
+      <div className="flex flex-wrap justify-center items-center gap-4">
+        <div>
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </div>
+        <div className="p-4 text-7xl">
+          <TbBrandNextjs />
+        </div>
+        <div className="p-4 text-7xl text-green-500">
+          <SiMongodb />
+        </div>
+        <div className="p-4 text-7xl text-red-700">
+          <DiRedis />
+        </div>
+        <div className="p-4 text-7xl text-green-400">
+          <FaNodeJs />
+        </div>
+
+        <div className="text-7xl text-blue-400">
+          <RiTailwindCssFill />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Technology;
